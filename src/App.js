1beba39c5ea2eb001e0cb0e1a5fd8139bde00208
Marcelo Cortes <marcelocortes@pop-os.localdomain>
2020-02-24 16:10:54 -0300

    board and cards components

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import React from 'react';
 
 import GlobalStyle from './styles/global';
 
+import Header from './components/Header';
+import Board from './components/Board';
+
 function App() {
   return (
-    <div className="App">
+    <>
+      <Header></Header>
+      <Board></Board>
       <GlobalStyle></GlobalStyle>
-    </div>
+    </>
   );
 }
 
---
+++ src/components/Card/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Container, Label } from './styles';
+
+export default function Card() {
+  return (
+    <Container>
+      <header>
+        <Label color="#7159c1"></Label>
+      </header>
+      <p>Fazer migração do servidor</p>
+      <img src="https://api.adorable.io/avatars/59/abott@adorable" alt=""></img>
+    </Container>
+  );
+}
